0fdbb4a23ac6d869ccdbd36cc764a1fa51eee87e
sofiulaloe <sahasofiulalom3@gmail.com>
2023-02-08 01:01:27 +0600

    add psot api commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app =express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId,  } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const port=process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.get('/', (req, res)=>{
     res.send('cake service server runing')
 })
@@ -18,12 +18,28 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
       const cakedeliverycollection=  client.db('cakedelivery').collection('services');
+      const cakeOrdercollection = client.db('cakedelivery').collection('orders');
+
       app.get('/services', async(req, res)=>{
          const query={};
          const cursor=  cakedeliverycollection.find(query);
          const result=await cursor.toArray()
          res.send(result)
         
+      });
+
+     app.get('/services/:id', async(req, res)=> {
+          const id =req.params.id;
+          const query={ _id: new  ObjectId(id) }
+          const service=await cakedeliverycollection.findOne(query)
+          res.send(service)
+      });
+
+        
+     app.post('/orders', async(req, res)=>{
+         const order =req.body;
+         const result= await cakeOrdercollection.insertOne(order)
+         res.send(result)
       })
     }
     finally{
