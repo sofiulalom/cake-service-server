42dcd089fe01de5376928abc1bb0cee531fe7809
sofiulaloe <sahasofiulalom3@gmail.com>
2023-02-11 00:22:27 +0600

    add jwt api commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app =express();
 const { MongoClient, ServerApiVersion, ObjectId, OrderedBulkOperation,  } = require('mongodb');
 require('dotenv').config();
+const jwt=require('jsonwebtoken')
 const cors = require('cors');
 const port=process.env.PORT || 5000;
 
@@ -19,7 +20,13 @@ async function run(){
     try{
       const cakedeliverycollection=  client.db('cakedelivery').collection('services');
       const cakeOrdercollection = client.db('cakedelivery').collection('orders');
-
+      // jwt api 
+      app.post('/jwt', (req, res)=> {
+          const user=req.body;
+          const token= jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+          res.send({token})
+      })
+      // services api 
       app.get('/services', async(req, res)=>{
          const query={};
          const cursor=  cakedeliverycollection.find(query);
@@ -27,7 +34,7 @@ async function run(){
          res.send(result)
         
       });
-
+       // services api id 
      app.get('/services/:id', async(req, res)=> {
           const id =req.params.id;
           const query={ _id: new  ObjectId(id) }
